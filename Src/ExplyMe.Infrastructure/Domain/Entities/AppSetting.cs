cdf64bc48f1e3abcaaf05b87f63a2d349b7d96d8
Yago dos Santos Rocha <yagosrocha1@gmail.com>
2021-01-16 16:32:11 -0300

    Criando serviço para buscar chaves tokenizadas

---
+++ Src/ExplyMe.Infrastructure/Domain/Entities/AppSetting.cs
@@ -0,0 +1,10 @@
+﻿using ExplyMe.Infrastructure.Data;
+
+namespace ExplyMe.Infrastructure.Domain.Entities
+{
+    public class AppSetting : Entity
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Src/ExplyMe.Infrastructure/Services/AppSettingsService.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using ExplyMe.Infrastructure.Data;
+using ExplyMe.Infrastructure.Domain.Entities;
+using ExplyMe.Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExplyMe.Infrastructure.Services
+{
+    public class AppSettingsService : IAppSettingsService
+    {
+        public ISqlConnectionFactory SqlConnectionFactory { get; }
+        public IMemoryCache MemoryCache { get; }
+        private const string CacheKey = "APP_SETTINGS";
+
+        public AppSettingsService(
+            ISqlConnectionFactory sqlConnectionFactory,
+            IMemoryCache memoryCache)
+        {
+            SqlConnectionFactory = sqlConnectionFactory ?? throw new ArgumentNullException(nameof(sqlConnectionFactory));
+            MemoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
+        }
+
+        public string Get(string key)
+        {
+            var appSettingsFromCache = MemoryCache.Get<IEnumerable<AppSetting>>(CacheKey);
+
+            if (appSettingsFromCache is null)
+                appSettingsFromCache = SeedSettings();
+
+            return appSettingsFromCache.FirstOrDefault(x => x.Key == key)?.Value;
+        }
+
+        public IEnumerable<AppSetting> SeedSettings()
+        {
+            using var connection = SqlConnectionFactory.CreateReadConnection();
+            var query = @"SELECT * FROM [AppSettings]";
+            var result = connection.Query<AppSetting>(query);
+
+            MemoryCache.Set(CacheKey, result);
+
+            return result;
+        }
+    }
+}
