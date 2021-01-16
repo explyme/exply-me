f61b7a301bae8a1339e98a3ea90abb5431565845
Yago dos Santos Rocha <yagosrocha1@gmail.com>
2021-01-16 00:12:31 -0300

    Ajustes

--- Src/ExplyMe/Program.cs
+++ Src/ExplyMe/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace ExplyMe
+namespace ExplyMe.WebApp
 {
     public class Program
     {
--- Src/ExplyMe/Startup.cs
+++ Src/ExplyMe/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ExplyMe
+namespace ExplyMe.WebApp
 {
     public class Startup
     {
