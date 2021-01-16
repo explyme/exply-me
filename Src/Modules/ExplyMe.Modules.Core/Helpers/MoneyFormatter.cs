5406b981444556e32cf517712a86a407a138ec38
Yago dos Santos Rocha <yagosrocha1@gmail.com>
2021-01-16 13:36:56 -0300

    Criando helper para formatar dinheiro

---
+++ Src/Modules/ExplyMe.Modules.Core/Helpers/MoneyFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExplyMe.Modules.Core.Helpers
+{
+    public static class MoneyFormatter
+    {
+        public static string Format(long amount)
+        {
+            return amount > 0 ? string.Format("{0:#.00}", Convert.ToDecimal(amount) / 100) : "0,00";
+        }
+    }
+}
