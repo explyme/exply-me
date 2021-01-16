c2e10f3f3628a7d111968fd869118d64e1b30507
Yago dos Santos Rocha <yagosrocha1@gmail.com>
2021-01-16 16:36:56 -0300

    Criando salas de vídeo

--- Src/Modules/ExplyMe.Modules.Call/ModuleInitializer.cs
+++ Src/Modules/ExplyMe.Modules.Call/ModuleInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using ExplyMe.Infrastructure.Modules;
+using ExplyMe.Infrastructure.Services.Interfaces;
 using ExplyMe.Modules.Call.Data;
 using ExplyMe.Modules.Call.Data.Interfaces;
 using ExplyMe.Modules.Call.Services;
@@ -6,6 +7,7 @@ using ExplyMe.Modules.Call.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Twilio;
 
 namespace ExplyMe.Modules.Call
 {
@@ -13,6 +15,11 @@ namespace ExplyMe.Modules.Call
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var appSettingsService = app.ApplicationServices.GetService<IAppSettingsService>();
+            var accountSid = appSettingsService.Get("TwilioAccountSid");
+            var authToken = appSettingsService.Get("TwilioAuthToken");
+
+            TwilioClient.Init(accountSid, authToken);
         }
 
         public void ConfigureServices(IServiceCollection services)
