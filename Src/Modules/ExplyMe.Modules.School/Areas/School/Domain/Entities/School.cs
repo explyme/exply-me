287e137aad1aa508a34019ad04648472fbd1cdb8
Diego Gomes Soares <dsoares@braspag.com.br>
2021-01-16 01:38:49 -0300

    criando entidades da escola

---
+++ Src/Modules/ExplyMe.Modules.School/Areas/School/Domain/Entities/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExplyMe.Modules.School.Areas.School.Domain.Entities
+{
+    public class School
+    {
+        public School()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+        public long Id { get; set; }
+        public string FriendlyUrl{ get; set; }
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string LogoUrl { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsBlocked { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ Src/Modules/ExplyMe.Modules.School/Areas/School/Domain/Entities/SchoolUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExplyMe.Modules.School.Areas.School.Domain.Entities
+{
+    public class SchoolUser
+    {
+        public SchoolUser()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+        public long SchoolId { get; set; }
+        public long UserId { get; set; }
+        public bool IsInstructor { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
