c3890459ebd0b079eb37e64301302243faf63dde
Yago dos Santos Rocha <yagosrocha1@gmail.com>
2021-01-15 21:12:33 -0300

    Criando controller de account para o login

---
+++ Src/Modules/ExplyMe.Modules.Core/Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplyMe.Modules.Core.Areas.Account.Controllers
+{
+    [Area("account")]
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
