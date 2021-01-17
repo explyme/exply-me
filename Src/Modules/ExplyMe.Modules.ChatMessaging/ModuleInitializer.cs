4fc418b903b672c1eecf39a6bfed82b545e5a973
Diego Gomes Soares <dsoares@braspag.com.br>
2021-01-16 22:30:31 -0300

    adicionando modulo de chat

---
+++ Src/Modules/ExplyMe.Modules.ChatMessaging/ModuleInitializer.cs
@@ -0,0 +1,22 @@
+﻿using ExplyMe.Infrastructure.Modules;
+using ExplyMe.Modules.ChatMessaging.Services;
+using ExplyMe.Modules.ChatMessaging.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExplyMe.Modules.ChatMessaging
+{
+    public class ModuleInitializer : IModuleInitializer
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IMessageCreator, MessageCreator>();
+            services.AddTransient<IMessageFinder, MessageFinder>();
+        }
+    }
+}
---
+++ Src/Modules/ExplyMe.Modules.ChatMessaging/Services/Interfaces/IMessageFinder.cs
@@ -0,0 +1,11 @@
+﻿using ExplyMe.Modules.ChatMessaging.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExplyMe.Modules.ChatMessaging.Services.Interfaces
+{
+    public interface IMessageFinder
+    {
+        Task<IEnumerable<MessageEntity>> FindAllByFromUserIdAndToUserId(long fromUserId, long toUserId);
+    }
+}
