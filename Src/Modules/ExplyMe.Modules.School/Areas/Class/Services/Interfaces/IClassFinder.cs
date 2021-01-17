9621efc54c84dbba914ef12766d3194fa87b1b62
Taís Vianna <38465611+tvianna@users.noreply.github.com>
2021-01-16 23:38:01 -0300

    adicionando visualização de turma

---
+++ Src/Modules/ExplyMe.Modules.School/Areas/Class/Services/Interfaces/IClassFinder.cs
@@ -0,0 +1,12 @@
+﻿using ExplyMe.Modules.School.Areas.Class.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExplyMe.Modules.School.Areas.Class.Services.Interfaces
+{
+    public interface IClassFinder
+    {
+        Task<ClassEntity> FindByIdAsync(long classId);
+        Task<IEnumerable<ClassEntity>> FindAllAsync();
+    }
+}
